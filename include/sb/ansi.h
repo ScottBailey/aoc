0ed9ab8f9e5732841e26c99e997ad164dd9b37fb
Scott Bailey <sbailey@inter-coastal.net>
2020-12-26 20:05:12 -0700

    add a target for the tests

---
+++ include/sb/ansi.h
@@ -0,0 +1,20 @@
+#ifndef sb_ansi_h
+#define sb_ansi_h
+
+namespace sb {
+
+
+const std::string black{"\u001b[30m"};
+const std::string red{"\u001b[31m"};
+const std::string green{"\u001b[32m"};
+const std::string yellow{"\u001b[33m"};
+const std::string blue{"\u001b[34m"};
+const std::string magenta{"\u001b[35m"};
+const std::string cyan{"\u001b[36m"};
+const std::string white{"\u001b[37m"};
+const std::string reset{"\u001b[0m"};
+
+
+} // namespace sb
+
+#endif
