9ac56baf572500e0e41fe689e1f80c03f9e279cb
Scott Bailey <sbailey@inter-coastal.net>
2021-01-03 12:51:23 -0700

    2015 day 12 part2

--- 2015/12/main.cpp
+++ 2015/12/main.cpp
@@ -40,7 +40,7 @@ void part1(const container_t& str)
    size_t pos =0;
    while(pos < str.size())
    {
-      // find the first char
+      // find the first char of a number
       while(pos < str.size() && (str[pos] < '0' || str[pos] > '9'))
          ++pos;
       if(pos > str.size())
@@ -64,11 +64,105 @@ void part1(const container_t& str)
 }
 
 
-void part2(const container_t& list)
+void part2(const container_t& in)
 {
-   int result = list.size();
+   int64_t sum = 0;
 
-   std::cout << sb::white << "Part 2: " << sb::reset << result << "\n";
+   std::string str = in;
+
+   // remove objects containing red
+   size_t pos =0;
+   while(pos < str.size())
+   {
+      if(pos > str.size())
+         break;
+
+      if(str[pos] != 'r')
+      {
+          ++pos;
+          continue;
+      }
+      if(pos == 0 || (pos+3) > str.size() || std::string_view(str.data()+pos-1,5) != "\"red\"")
+      {
+         ++pos;
+         continue;
+      }
+
+      //found red, now find if it's an array '[' or object '{'
+      size_t rloc = pos;
+      size_t acount=0;
+      size_t ocount=0;
+      bool is_array = false;
+      while(pos < str.size())
+      {
+         if(str[pos] == '[')
+         {
+            if(!acount)
+            {
+               is_array = true;
+               break;
+            }
+            --acount;
+         }
+         else if(str[pos] == ']')
+            ++acount;
+         else if(str[pos] == '{')
+         {
+            if(!ocount)
+               break;
+            --ocount;
+         }
+         else if(str[pos] == '}')
+            ++ocount;
+         --pos;
+      }
+      if(is_array)
+      {
+         pos = rloc + 1;
+         continue;
+      }
+      // object, find the terminator
+      size_t first = pos;
+      size_t count = 1;
+      pos = rloc+1;
+      while(count && pos < str.size())
+      {
+         if(str[pos] == '{')
+            ++count;
+         else if(str[pos] == '}')
+            --count;
+         ++pos;
+      }
+      std::clog << "dumping: " << std::string_view(str.data()+first, pos-first) << "\n" << std::flush;
+      str.erase(first,pos-first);
+      pos = first;
+   }
+
+   // now count the numbers
+   pos =0;
+   while(pos < str.size())
+   {
+      // find the first char of a number or red
+      while(pos < str.size() && (str[pos] < '0' || str[pos] > '9'))
+         ++pos;
+      if(pos > str.size())
+         break;
+
+      size_t first = pos;
+      if(pos > 0 && str[pos-1] == '-')
+         --first;
+
+      while(pos < str.size() && (str[pos] >= '0' && str[pos] <= '9'))
+         ++pos;
+
+      int64_t temp=0;
+      if(!sb::string::from( std::string_view(str.data()+first, pos-first), temp))
+         throw -1;
+
+      sum += temp;
+   }
+
+   std::cout << sb::white << "Part 2: " << sb::reset << sum << "\n";
 }
 
 
